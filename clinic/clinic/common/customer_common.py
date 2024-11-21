93790f1f875ce2b7654d6f6628a1ae9a494fd0b1
rashedpro <rashedpro2025@gmail.com>
2024-11-21 11:01:47 +0300

    fix bug in classify

--- clinic/clinic/common/customer_common.py
+++ clinic/clinic/common/customer_common.py
@@ -5,13 +5,12 @@ def get_customer_classify(customer_name):
     total_amount=0
     payments=frappe.db.get_all("Sales Invoice",filters={"customer":customer_name,"docstatus":1,"status":"Paid"},fields=["grand_total"])
     total_amount=sum(payment["grand_total"] for payment in payments)
-
-    if total_amount<=16000:
+    if total_amount<=15000:
 
         return {"classify":"New"}
-    elif  total_amount in range(16001,30000):
+    elif  15001<total_amount<29999 :
         return {"classify":"Regular"}
-    elif total_amount in range(30001,50000):
+    elif 30000<total_amount<49999:
         return {"classify":"VIP"}
     else:
         return {"classify":"VIP+"}
