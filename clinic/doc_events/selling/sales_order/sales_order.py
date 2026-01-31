ec9ddd35588ff5082596ba44cde9e0648106d6ce
kholoud21 <kholoudelbaroudy19@gmail.com>
2026-01-31 18:35:11 +0200

    sales order add tax for customer not saudi

--- clinic/doc_events/selling/sales_order/sales_order.py
+++ clinic/doc_events/selling/sales_order/sales_order.py
@@ -1,5 +1,26 @@
 import frappe
 
 @frappe.whitelist()
-def validate(doc,method = None):
-    pass
+def before_validate(doc,method = None):
+    add_tax(doc)
+
+
+def add_tax(doc):
+    if doc.client_appointment_ct and doc.is_new():
+        doc.taxes_and_charges = ""
+        # get customer id 
+        customer_id = frappe.get_value("Customer", {"name": doc.customer}, "id_no")
+        # chck if customer id starts with no != "1"
+        if customer_id and not customer_id.startswith("1"):
+            # get tax template 
+            tax_template = frappe.get_value("Sales Taxes and Charges Template",{"tax_category":["like","%15%"]},"name")
+            taxes = frappe.get_all("Sales Taxes and Charges",{"parent":tax_template},["charge_type","account_head","rate","description"])
+            if taxes:
+                # clear existing taxes
+                doc.taxes = []
+                for tax in taxes:
+                    tax_row = doc.append("taxes", {})
+                    tax_row.charge_type = tax.charge_type
+                    tax_row.account_head = tax.account_head
+                    tax_row.rate = tax.rate
+                    tax_row.description = tax.description
